b16460115954bf75819f7dbd85a08d8940b503e1
gamsteron <gamsteron@gmail.com>
2018-02-05 13:37:01 +0100

    Update AIO_GSO.lua

--- AIO_GSO.lua
+++ AIO_GSO.lua
@@ -1438,7 +1438,7 @@ function __gsoOrb:_orb(unit)
         self.endTime = endTime
     end
     
-    local canMove = _gso.Vars._canMove() and checkT > self.lAttack + self.windUpT + _gso.Farm.latency + 0.025 + self.menu.delays.windup:Value()*0.001
+    local canMove = _gso.Vars._canMove() and checkT > self.lAttack + self.windUpT + (_gso.Farm.latency*0.5) + 0.025 + self.menu.delays.windup:Value()*0.001
     local canAA = _gso.Vars._canAttack() and self.isBlinded == false and self.canAA and canMove and checkT > self.endTime - 0.034 - (_gso.Farm.latency*1.5)
     local isTarget = unit ~= nil
     
